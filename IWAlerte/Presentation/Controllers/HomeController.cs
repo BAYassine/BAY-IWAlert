ccfa5003fe834c80c10850e9061ab5ec8eee0a24
eloss <eloss@LAPTOP-44SCNDAL>
2018-12-09 06:44:49 +0100

    Commit ahmed

--- IWAlerte/Presentation/Controllers/HomeController.cs
+++ IWAlerte/Presentation/Controllers/HomeController.cs
@@ -10,7 +10,40 @@ namespace Presentation.Controllers
     {
         public ActionResult Index()
         {
+            IWContext context = new IWContext();
+            List<Models.Alerte> alts = context.Alertes.Include("Disease").Include("User").ToList<Models.Alerte>();
+            ViewBag.result = alts;
+
+
+
+
             return View();
+        }
+
+        public JsonResult getColors()
+        {
+            IWContext context = new IWContext();
+
+            //var count =context.Alertes.Include("Disease").Include("User").GroupBy(n => n.User.Place);
+            List<Models.PaysCouleur> list = new List<Models.PaysCouleur>();
+            foreach (var line in context.Alertes.GroupBy(info => info.User.Place.Country)
+                        .Select(group => new
+                        {
+                            Metric = group.Key,
+                            Count = group.Count()
+                        }).ToList())
+            {
+
+                string couleur = "";
+                if (line.Count < 5) couleur = "#F9573B";
+                if (line.Count > 5) couleur = "#FF9900";
+                if (line.Count > 10) couleur = "#FF9933";
+                if (line.Count > 15) couleur = "#FF0000";
+
+                list.Add(new Models.PaysCouleur { Pays = line.Metric, couleur = couleur });
+            }
+
+            return new JsonResult { Data = list, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
         public ActionResult About()
